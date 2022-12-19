e109294c7fda369555fd5cc161528d04fa68bbfc
Amol Jandial <amoljandial1@gmail.com>
2022-12-19 05:53:13 +0530

    Clipboard and Candidate view added

--- app/src/main/java/com/example/accessibilitykeyboardapp/KeyboardIME.java
+++ app/src/main/java/com/example/accessibilitykeyboardapp/KeyboardIME.java
@@ -7,24 +7,25 @@ import android.inputmethodservice.InputMethodService;
 import android.inputmethodservice.Keyboard;
 import android.inputmethodservice.KeyboardView;
 import android.util.Log;
-import android.view.KeyEvent;
-import android.view.MenuItem;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputConnection;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
-import java.security.Key;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-public class KeyboardIME extends InputMethodService implements KeyboardView.OnKeyboardActionListener
-         {
+public class KeyboardIME extends InputMethodService implements KeyboardView.OnKeyboardActionListener,
+        View.OnClickListener, ClipboardManager.OnPrimaryClipChangedListener {
 
+    String pasteData = "";
     private CustomKeyboardView kv;
     private Keyboard keyboard;
-    String pasteData = "";
-//             ClipboardManager.OnPrimaryClipChangedListener
+    private Button btn;
+
+    private CandidateView mCandidateView;
+
     @Override
     public View onCreateInputView() {
         kv = (CustomKeyboardView) getLayoutInflater().inflate(R.layout.keyboard_view, null);
@@ -32,15 +33,36 @@ public class KeyboardIME extends InputMethodService implements KeyboardView.OnKe
         kv.setKeyboard(keyboard);
         kv.setOnKeyboardActionListener(this);
 
-//        ClipboardManager clipBoard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
-//        clipBoard.addPrimaryClipChangedListener(this);
-//        clipboard();
+        ClipboardManager clipBoard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+        clipBoard.addPrimaryClipChangedListener(this);
         return kv;
     }
 
+    @Override
+    public View onCreateCandidatesView() {
+        View wordBar = (LinearLayout) getLayoutInflater().inflate(R.layout.wordbar, null);
+        LinearLayout ll = (LinearLayout) wordBar.findViewById(R.id.wordsLayout);
+        btn = (Button) wordBar.findViewById(R.id.clipboard_button);
+        btn.setOnClickListener(this);
+        mCandidateView = new CandidateView(this);
+        mCandidateView.setService(this);
+        setCandidatesViewShown(true);
+        mCandidateView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT));
+        ll.addView(mCandidateView);
+        clipboard();
+        return ll;
+    }
 
+    @Override
+    public void onComputeInsets(Insets outInsets) {
+        super.onComputeInsets(outInsets);
+        if (!isFullscreenMode()) {
+            outInsets.contentTopInsets = outInsets.visibleTopInsets;
+        }
+    }
 
-             @Override
+    @Override
     public void onPress(int primaryCode) {
 
     }
@@ -54,8 +76,8 @@ public class KeyboardIME extends InputMethodService implements KeyboardView.OnKe
     public void onKey(int primaryCode, int[] keyCodes) {
         InputConnection ic = getCurrentInputConnection();
 
-        switch(primaryCode){
-            case Keyboard.KEYCODE_DELETE :
+        switch (primaryCode) {
+            case Keyboard.KEYCODE_DELETE:
                 kv.deleteText(ic);
                 break;
             case Keyboard.KEYCODE_SHIFT:
@@ -67,10 +89,9 @@ public class KeyboardIME extends InputMethodService implements KeyboardView.OnKe
 
             default:
 
-                if(primaryCode == 10001){
-                   kv.changeKeyboardLayout(R.xml.number_symbols_layout);
-                }
-                else if(primaryCode == 10002){
+                if (primaryCode == 10001) {
+                    kv.changeKeyboardLayout(R.xml.number_symbols_layout);
+                } else if (primaryCode == 10002) {
                     kv.changeKeyboardLayout(R.xml.qwerty_layout);
                 }
 //                else if(primaryCode == 10004){
@@ -90,33 +111,21 @@ public class KeyboardIME extends InputMethodService implements KeyboardView.OnKe
 //                        }
 //                    }
 //                }
-                else{
+                else {
                     kv.publishText(ic, primaryCode);
                 }
         }
     }
 
-//    private void clipboard(){
-//        ClipboardManager clipboardManager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-//        ClipData.Item item = clipboardManager.getPrimaryClip().getItemAt(0);
-//        pasteData = (String) item.getText();
-//        Keyboard currentKeyboard = kv.getKeyboard();
-//        List<Keyboard.Key> keys = currentKeyboard.getKeys();
-//        Log.d("clipboard bitch", "clipboard Data: "+pasteData);
-//
-//        for(Keyboard.Key key : keys){
-//            if(key.label.equals("")){
-//                key.label = pasteData;
-//                kv.invalidateAllKeys();
-//            }
-//        }
-//
-//    }
-
-
-
-
-
+    private void clipboard(){
+       btn.setVisibility(View.VISIBLE);
+        ClipboardManager clipboardManager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData.Item item = clipboardManager.getPrimaryClip().getItemAt(0);
+        Log.d("bitches", "clipboard: "+ item.getText());
+        if(item.getText() != null){
+            btn.setText(item.getText());
+        }
+    }
 
 
     @Override
@@ -144,9 +153,20 @@ public class KeyboardIME extends InputMethodService implements KeyboardView.OnKe
 
     }
 
-//    @Override
-//    public void onPrimaryClipChanged() {
-//        Log.d("clipboard", "onPrimaryClipChanged: ");
-//        clipboard();
-//    }
+    @Override
+    public void onClick(View v) {
+        Button btn = (Button) v;
+        String text = (String) btn.getText();
+        getCurrentInputConnection().commitText(text, 1);
+        ClipData clip = ClipData.newPlainText("","");
+        ClipboardManager clipboardManager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        clipboardManager.setPrimaryClip(clip);
+        btn.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onPrimaryClipChanged() {
+        Log.d("clipboard", "onPrimaryClipChanged: ");
+        clipboard();
+    }
 }
